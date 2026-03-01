28d9811e8394a8f6c77cd8934e7ff69434c8f8f3
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2026-03-01 23:18:11 +0200

    Added serviceses

---
+++ src/main/java/org/project/lab/service/UniversityService.java
@@ -0,0 +1,37 @@
+package org.project.lab.service;
+
+import org.project.lab.dao.UniversityDAO;
+import org.project.lab.dao.UniversityDAOJPA;
+import org.project.lab.entity.University;
+import org.project.lab.interceptor.Logged;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@RequestScoped
+@Logged
+public class UniversityService {
+
+  @Inject
+  private UniversityDAOJPA universityDAO;
+
+  public List<University> getAllUniversities() {
+    return universityDAO.findAll();
+  }
+
+  public University getUniversity(Long id) {
+    return universityDAO.find(id);
+  }
+
+  @Transactional
+  public void addUniversity(University university) {
+    universityDAO.persist(university);
+  }
+
+  @Transactional
+  public University updateUniversity(University university) {
+    return universityDAO.merge(university);
+  }
+}
